2dde2e5bafc2a302e1facd1292a364faac00ad73
Tylerz3936 <tylerzhao103@gmail.com>
2024-07-14 15:09:00 -0400

    trying to fix deployment issue-added a copy of the index.html in the root directory

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-
 const dbPath = path.join(__dirname, 'db', 'db.json');
 
 // Promisify readFile and writeFile
