ab64ddbd0a09b3259e7093f82f82e1d0fd572fb4
Tylerz3936 <tylerzhao103@gmail.com>
2024-07-20 18:10:02 -0400

    revert changes moving index.html back to public

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // HTML Routes
 
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
